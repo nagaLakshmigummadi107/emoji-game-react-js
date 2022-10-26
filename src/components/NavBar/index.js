96470d152b4e72cd6ec5e4e442771369d8bd72af
Naga Lakshmi <nagalakshmi.g107@gmail.com>
2022-10-26 10:03:10 +0000

    emoji game react js

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,39 @@
+import './index.css'
+
+const NavBar = props => {
+  const {currentScore, isGameInProgress, topScore} = props
+
+  return (
+    <nav className="nav-bar-container">
+      <div className="title-with-score-container">
+        <div className="logo-and-title-container">
+          <img
+            className="emoji-logo"
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+          />
+          <h1 className="title">Emoji Game</h1>
+          <p className="my-text">
+            WebApp built by Tenali Bhargav. <br />
+            To see my LinkedIn Profile{' '}
+            <a
+              href="https://www.linkedin.com/in/bhargavtenali/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Click Here
+            </a>
+          </p>
+        </div>
+        {isGameInProgress && (
+          <div className="scores-container">
+            <p className="score">Score: {currentScore}</p>
+            <p className="score">Top Score: {topScore}</p>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
